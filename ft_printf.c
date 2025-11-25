b8d9d1c94a89ab71de333fd42fa8fa61aa54aca4
Basma Cherguaoui <cherguaouibasma93@gmail.com>
2025-11-25 11:59:26 +0100

    last_update

--- ft_printf.c
+++ ft_printf.c
@@ -1,61 +1,79 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bchergua <bchergua@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/22 12:04:30 by bchergua          #+#    #+#             */
-/*   Updated: 2025/11/23 01:50:45 by bchergua         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_printf.h"
 
-static int	ft_handle_format(char c, va_list args)
+static int ver(const char *s)
 {
-	if (c == 'c')
-		return (ft_putchar(va_arg(args, int)));
-	if (c == 's')
-		return (ft_putstr(va_arg(args, char *)));
-	if (c == 'p')
-		return (ft_putpt(va_arg(args, unsigned long)));
-	if (c == 'd' || c == 'i')
-		return (ft_putnbr(va_arg(args, int)));
-	if (c == 'u')
-		return (ft_put_unsigned(va_arg(args, unsigned int)));
-	if (c == 'x' || c == 'X')
-		return (ft_puthex(va_arg(args, unsigned int), c));
-	if (c == '%')
-		return (ft_putchar('%'));
-	return (0);
+    if (*s == 'c' || *s == 's' || *s == 'p'
+        || *s == 'd' || *s == 'i' || *s == 'u'
+        || *s == 'x' || *s == 'X' || *s == '%')
+        return (1);
+    return (0);
 }
 
-int	ft_printf(const char *format, ...)
+static int ft_handle_format(char c, va_list args)
 {
-	va_list	args;
-	int		count;
-	int		i;
-
-	if (!format)
-		return (-1);
-	va_start(args, format);
-	count = 0;
-	i = 0;
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			count += ft_handle_format(format[i], args);
-			i++;
-		}
-		else
-		{
-			count += ft_putchar(format[i]);
-			i++;
-		}
-	}
-	va_end(args);
-	return (count);
+    if (c == 'c')
+        return (ft_putchar(va_arg(args, int)));
+    if (c == 's')
+        return (ft_putstr(va_arg(args, char *)));
+    if (c == 'p')
+        return (ft_putpt(va_arg(args, unsigned long)));
+    if (c == 'd' || c == 'i')
+        return (ft_putnbr(va_arg(args, int)));
+    if (c == 'u')
+        return (ft_put_unsigned(va_arg(args, unsigned int)));
+    if (c == 'x' || c == 'X')
+        return (ft_puthex(va_arg(args, unsigned int), c));
+    if (c == '%')
+        return (ft_putchar('%'));
+    return (0);
 }
+
+static int percent(const char *format, int *i, va_list args)
+{
+    int count;
+
+    count = 0;
+    if (!format[*i + 1])
+        return (-1);
+    if (ver(&format[*i + 1]))
+    {
+        (*i)++;
+        count += ft_handle_format(format[*i], args);
+    }
+    else
+    {
+        count += ft_putchar('%');
+        count += ft_putchar(format[*i + 1]);
+        (*i)++;
+    }
+    return (count);
+}
+
+int ft_printf(const char *format, ...)
+{
+    va_list args;
+    int     count;
+    int     i;
+
+    if (!format || write(1, "", 0) < 0)
+        return (-1);
+    va_start(args, format);
+    count = 0;
+    i = 0;
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            int r = percent(format, &i, args);
+            if (r == -1)
+                return (va_end(args), -1);
+            count += r;
+        }
+        else
+            count += ft_putchar(format[i]);
+        i++;
+    }
+    va_end(args);
+    return (count);
+}
+
--- main.c
+++ main.c
@@ -1,78 +1,53 @@
-#include <stdio.h>
 #include "ft_printf.h"
-
-int main(void)
+#include <stdio.h>
+#include <fcntl.h>
+//            SOME CRAZY TESTS
+int main () 
 {
-	int r1, r2;
-	int x = 42;
+    int x = 42;
+    
+  int j = ft_printf("un test de ft_printf: --%d-- --%s-- --%u-- --%p-- \n",0, NULL, 0, NULL);
+	int i = printf("un test de ft_printf: --%d-- --%s-- --%u-- --%p-- \n",0, NULL, 0, NULL);
+	printf("ft_printf : ret = %d ||||||||| printf : ret = %d \n",j,i);
+    
+  int a = ft_printf("ttt%cttt%d\n");
+  ft_printf("%d\n", a);
+  printf("\n");
+  int b = printf("ttt%cttt%d\n");
+  printf("%d\n", b);
 
-	// Characters
-	r1 = printf("%c\n", 'A');
-	r2 = ft_printf("%c\n", 'A');
-	printf("ret: %d | %d\n\n", r1, r2);
+  int a = ft_printf("%d\n",-45);
+  ft_printf("%d\n", a);
+  printf("\n");
+  int b = printf("%d\n",-45);
+  printf("%d\n", b);
+    
+  int a = ft_printf("Test %% simple : %%\n");
+  ft_printf("%d\n", a);
+   
+  int c = printf(NULL);//if format == NULL
+  printf("%d\n", c);
+    
+  fclose(stdout); // if stdout is close
+  int a = ft_printf("fedyhew");
+  fprintf(stderr,"%d\n",a);
+    
+  printf("\ncomparaison : \n\n");
+    
+  int i = printf("un test de ft_printf: --%d-- --%s-- --%u-- --%p-- \n",0, NULL, 0, NULL);
+  printf("%d%%\n", i);
+  printf("Test %% simple : %%\n");
+  int a =ft_printf("%rey");
+	ft_printf("%d\n",a);
+	printf("%%%\n");
 
-	// Strings
-	r1 = printf("%s\n","basma");
-	r2 = ft_printf("%s\n","basma");
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%s\n", "");
-	r2 = ft_printf("%s\n", "");
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	// Integers
-	r1 = printf("%d\n", 42);
-	r2 = ft_printf("%d\n", 42);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%d\n", (int)-2147483648);
-	r2 = ft_printf("%d\n", (int)-2147483648);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%i\n", 0);
-	r2 = ft_printf("%i\n", 0);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	// Unsigned
-	r1 = printf("%u\n", 0u);
-	r2 = ft_printf("%u\n", 0u);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%u\n", 4294967295u);
-	r2 = ft_printf("%u\n", 4294967295u);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	// Hexadecimal
-	r1 = printf("%x\n", 255);
-	r2 = ft_printf("%x\n", 255);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%X\n", 255);
-	r2 = ft_printf("%X\n", 255);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%x\n", 0);
-	r2 = ft_printf("%x\n", 0);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	// Pointers
-	r1 = printf("%p\n", &x);
-	r2 = ft_printf("%p\n", &x);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	r1 = printf("%p\n", NULL);
-	r2 = ft_printf("%p\n", NULL);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	// Percent
-	r1 = printf("%%\n");
-	r2 = ft_printf("%%\n");
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	// Mixed
-	r1 = printf("%c %s %d %u %x %p %%\n", 'A', "test", -42, 100u, 255, &x);
-	r2 = ft_printf("%c %s %d %u %x %p %%\n", 'A', "test", -42, 100u, 255, &x);
-	printf("ret: %d | %d\n\n", r1, r2);
-
-	return 0;
+  
+  int a = ft_printf("%%%%%l");
+  printf("\n");
+  printf("%d\n", a);
+  printf("abdoul  ||||  %%%%");
+  write(1," A ",3);
+  int a = ft_printf("");
+  printf("%d",a);
+  return 0;
 }
